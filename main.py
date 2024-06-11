91e25bf16e40c0d4a06a156a25a6b8f7ed8368b0
lucas.andretti <andrettilucas9@gmail.com>
2024-06-10 21:18:36 -0300

    1.0

---
+++ main.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+def salvar_video (link_do_video):
+    print('Iniciando o download...')
+
+    try:
+        yt = YouTube (link_do_video, on_complete_callback=on_progress)
+        video_stream = yt.streams.get_highest_resolution()
+        video_stream.download()
+
+        return '\nDownload completo!'
+    except:
+        return 'Não foi possível baixar o vídeo'
+    
+if __name__ == '__main__':
+    while True:
+        link_do_video = input ('Informe o link do Youtube para baixar ou aperte Enter para encerrar o programa: ')
+
+        if link_do_video !='':
+            print(salvar_video(link_do_video))
+            continue
+        else:
+            print('Programa Encerrado!')
+            break
